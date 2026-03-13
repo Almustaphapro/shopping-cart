df28f5e8a89c99677978e3c214f3a308b4d98f2d
Almustaphapro <lawalm14@gmail.com>
2026-03-13 21:12:13 +0100

    second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./App.css";
 import Navbar from './components/Navbar';
+import { Shop } from "./pages/shop/shop";
+import { Cart } from "./pages/cart/cart";
+import { ShopContextProvider } from './context/shop-context';
+
 function App() {
   return (
     <div className='App'>
-      <Router>
+      <ShopContextProvider>
+        <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
-          <Route path="/cart" element={<h1>About</h1>} />
-          <Route path="/contact" element={<h1>Contact</h1>} />
-        </Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} /> 
+          </Routes>
       </Router>
+      </ShopContextProvider>
     </div>
   )
 }
---
+++ src/products.js
@@ -0,0 +1,60 @@
+import product1 from "./assets/iphone.webp";
+import product2 from "./assets/macbook.webp";
+import product3 from "./assets/canon.jpeg";
+import product4 from "./assets/ps5.webp";
+import product5 from "./assets/LED.jpeg";
+import product6 from "./assets/bag.jpeg";
+import product7 from "./assets/t-shirt.webp";
+import product8 from "./assets/hp.webp";
+export const PRODUCTS = [
+    {
+        id:1,
+        productName: "Iphone",
+        price: 750000,
+        productImage: product1
+    },
+      {
+        id:2,
+        productName: "Macbook Pro 2022" ,
+        price: 900000,
+        productImage: product2
+    },
+   {
+        id:3,
+        productName: "Canon M50 Camera", 
+        price: 500000,
+        productImage: product3
+    },
+     {
+        id:4,
+        productName: "ps5 Console", 
+        price: 830000,
+        productImage: product4
+    },
+
+    {
+        id:5,
+        productName: "LED Ligtht Strips", 
+        price: 830000,
+        productImage: product5
+    },
+    {
+        id:6,
+        productName: "Bag", 
+        price: 830000,
+        productImage: product6
+    },
+    {
+        id:7,
+        productName: "T-Shirt", 
+        price: 830000,
+        productImage: product7
+    },
+    {
+        id:8,
+        productName: "HP Laptop", 
+        price: 830000,
+        productImage: product8 
+    },
+    
+]
